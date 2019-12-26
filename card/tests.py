360fd67eac8e3a5fd3dfbb7c15ad843b85dee662
Ravi Indorey <ravi.indorey90@gmail.com>
2019-12-26 18:59:37 +0530

    Add card model-form

--- card/tests.py
+++ card/tests.py
@@ -1,6 +1,10 @@
 from django.test import TestCase
 from selenium import webdriver
 from .models import Card
+from .forms import CardForm
+
+VALID_CARD_NUMBER = 'GR96 0810 0010 0000 0123 4567 890'
+
 
 class FunctionalTests(TestCase):
     def setUp(self):
@@ -20,6 +24,13 @@ class UnitTest(TestCase):
         card = Card()
         total_cards = Card.objects.count()
         card.card_name = 'ABC Bank'
-        card.card_number = 'GR96 0810 0010 0000 0123 4567 890'
+        card.card_number = VALID_CARD_NUMBER
         card.save()
         self.assertEqual(Card.objects.count(), total_cards + 1)
+
+    def test_card_form_is_valid(self):
+        card_form = CardForm(data={
+            'card_name': 'ABC Bank',
+            'card_number': VALID_CARD_NUMBER
+            })
+        self.assertTrue(card_form.is_valid())
