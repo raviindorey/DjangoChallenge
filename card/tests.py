edd82e569fac3a25302f90a15a8028c1effda3f1
Ravi Indorey <ravi.indorey90@gmail.com>
2019-12-26 19:23:28 +0530

    Add view to handle submit form

--- card/tests.py
+++ card/tests.py
@@ -3,6 +3,7 @@ from selenium import webdriver
 from .models import Card
 from .forms import CardForm
 
+VALID_CARD_NAME = 'ABC Bank'
 VALID_CARD_NUMBER = 'GR96 0810 0010 0000 0123 4567 890'
 
 
@@ -23,14 +24,24 @@ class UnitTest(TestCase):
     def test_add_a_card(self):
         card = Card()
         total_cards = Card.objects.count()
-        card.card_name = 'ABC Bank'
+        card.card_name = VALID_CARD_NAME
         card.card_number = VALID_CARD_NUMBER
         card.save()
         self.assertEqual(Card.objects.count(), total_cards + 1)
 
-    def test_card_form_is_valid(self):
-        card_form = CardForm(data={
-            'card_name': 'ABC Bank',
+    def get_card_form(self):
+        return CardForm(data={
+            'card_name': VALID_CARD_NAME,
             'card_number': VALID_CARD_NUMBER
             })
+
+    def test_card_form_is_valid(self):
+        card_form = self.get_card_form()
         self.assertTrue(card_form.is_valid())
+
+    def test_card_form_submit_adds_a_card(self):
+        response = self.client.post('/cards', data={
+            'card_name': VALID_CARD_NAME,
+            'card_number': VALID_CARD_NUMBER,
+        })
+        self.assertEqual(response.status_code, 200)
--- card/views.py
+++ card/views.py
@@ -1,5 +1,27 @@
 from django.shortcuts import render
+from .forms import CardForm
+from .models import Card
 
 
 def card_list(request):
-    return render(request, 'cards/card_list.html', {'page': 'cards'})
+    cards = Card.objects.all()
+    if request.method == 'POST':
+        card_form = CardForm(data=request.POST)
+        if card_form.is_valid():
+            cd = card_form.cleaned_data
+            card_name = cd.get('card_name')
+            card_number = cd.get('card_number')
+            if card_name and card_number:
+                Card.objects.create(
+                    card_name=card_name,
+                    card_number=card_number
+                )
+
+    else:
+        card_form = CardForm()
+
+    return render(request, 'cards/card_list.html', {
+        'page': 'cards',
+        'cards': cards,
+        'card_form': card_form,
+    })
