e4ff255ee0bcb8b1f9de2ea5a792d421b2f22369
Ravi Indorey <ravi.indorey90@gmail.com>
2019-12-28 13:14:04 +0530

    Update settings.py

--- djangochallenge/settings.py
+++ djangochallenge/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False if os.environ['ENV'] == 'Production' else True
+DEBUG = False if os.environ.get('ENV', None) == 'Production' else True
 
 ALLOWED_HOSTS = []
 
