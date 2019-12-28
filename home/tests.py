4f25cabb6d619a79801e007f858b9ca3a28ceddc
Ravi Indorey <ravi.indorey90@gmail.com>
2019-12-28 13:48:03 +0530

    Add navigation tests for home

--- home/tests.py
+++ home/tests.py
@@ -11,5 +11,17 @@ class FunctionalTests(TestCase):
         self.browser.get('http://localhost:8000/')
         self.assertIn('The task is', self.browser.page_source)
 
+    def test_card_link_navigation_works(self):
+        self.browser.get('http://localhost:8000/')
+        self.browser.find_element_by_class_name('card-link').click()
+        self.assertIn('Add a new card', self.browser.page_source)
+
+    def test_address_link_navigation_works(self):
+        self.browser.get('http://localhost:8000/')
+        self.browser.find_element_by_class_name('address-link').click()
+        self.assertIn('Need an existing user first', self.browser.page_source)
+        self.assertIn('<input type="hidden" name="next" value="/address">',
+                      self.browser.page_source)
+
     def tearDown(self):
         self.browser.quit()
